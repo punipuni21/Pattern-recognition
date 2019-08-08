ef537ff88db0390c6896a0a8fef35e267a0b9438
punipuni21 <watanabe-ryotaro-yp@ynu.jp>
2019-08-08 09:41:04 +0900

    add posteriori-probability experiment codes

---
+++ 事後確率2.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 21:09:50 2019
+
+@author: ShimaLab
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 17:41:09 2019
+
+@author: ShimaLab
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+theta_list = [0.8,0.6,0.3]
+pi_list = [1/3,1/3,1/3]
+
+
+n = 10
+rr = np.array(list(range(0,11)))
+theta = np.array(theta_list)
+pi = np.array(pi_list)
+
+post_prob = np.zeros((3,11))
+
+for r in rr: 
+    
+    sum = 0
+    prob = np.zeros(3)
+    
+    for i in range(0,3):
+        
+        prob[i] = pi[i]*pow(theta[i],r)*pow((1-theta[i]),n-r)
+        sum += prob[i]
+    
+    #確率計算終了
+    
+    for i in range(0,3):
+         
+        post_prob[i][r] = prob[i]/sum
+    
+
+
+plt.plot(rr,post_prob[0],label='$\omega_{1}$')
+plt.plot(rr,post_prob[1],label='$\omega_{2}$')
+plt.plot(rr,post_prob[2],label='$\omega_{3}$')
+
+plt.xlabel("the number of head times")
+plt.ylabel("posteriori probability $P(\omega_{i}|x^{(10)})$")    
+plt.legend()
---
+++ 事後確率3.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 21:26:35 2019
+
+@author: ShimaLab
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 17:41:09 2019
+
+@author: ShimaLab
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+theta_list = [0.8,0.6,0.3]
+pi_list = [0.1,0.4,0.5]
+
+
+n = 100
+rr = np.array(list(range(0,101)))
+theta = np.array(theta_list)
+pi = np.array(pi_list)
+
+post_prob = np.zeros((3,101))
+
+for r in rr: 
+    
+    sum = 0
+    prob = np.zeros(3)
+    
+    for i in range(0,3):
+        
+        prob[i] = pi[i]*pow(theta[i],r)*pow((1-theta[i]),n-r)
+        sum += prob[i]
+    
+    #確率計算終了
+    
+    for i in range(0,3):
+         
+        post_prob[i][r] = prob[i]/sum
+    
+
+
+plt.plot(rr,post_prob[0],label='$\omega_{1}$')
+plt.plot(rr,post_prob[1],label='$\omega_{2}$')
+plt.plot(rr,post_prob[2],label='$\omega_{3}$')
+
+plt.xlabel("the number of head times")
+plt.ylabel("posteriori probability $P(\omega_{i}|x^{(10)})$")    
+plt.legend()
